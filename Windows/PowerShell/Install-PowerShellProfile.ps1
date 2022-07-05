320545c1b02fdaeac6eb29ac419a9833762ad33e
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2022-07-05 08:35:58 +0200

    Fix profile not installing when folder is not present.

--- Windows/PowerShell/Install-PowerShellProfile.ps1
+++ Windows/PowerShell/Install-PowerShellProfile.ps1
@@ -1,3 +1,8 @@
+<#
+    Installs the custom PowerShell profile at the default profile location.
+    This might need to run from an elevated shell.
+#>
+
 [string] $ProfileFileName = "Microsoft.PowerShell_profile.ps1"
 [string] $CustomProfilePath = "$PSScriptRoot\$ProfileFileName"
 if (-not (Test-Path $CustomProfilePath))
@@ -5,17 +10,19 @@ if (-not (Test-Path $CustomProfilePath))
     Write-Output "Could not find custom profile at '$CustomProfilePath'."
     return
 }
-# if (-not (Test-Path $ProfileDirectoryPath)) {
-#     Write-Output "Creating PowerShell profile folder at '$ProfileDirectoryPath'."
-#     New-Item -ItemType "Directory" -Path "$env:USERPROFILE\Documents\" -Name "PowerShell"
-# }
-[string] $destinationFolderPath = $profile | Split-Path
-# Default PowerShell profile file path.
-$destinationFilePath = "$destinationFolderPath\$ProfileFileName"
-Copy-Item -Path $CustomProfilePath -Destination $destinationFilePath
-Write-Output "Copied custom profile to '$destinationFilePath'."
 
-if (-not ($profile -eq $destinationFilePath))
+$defaultProfileDirectoryPath = "$env:USERPROFILE\Documents\PowerShell\"
+if (-not (Test-Path $defaultProfileDirectoryPath)) {
+    Write-Output "Creating PowerShell profile folder at '$defaultProfileDirectoryPath'."
+    New-Item -ItemType "Directory" -Path "$env:USERPROFILE\Documents\" -Name "PowerShell"
+}
+
+[string] $defaultProfileDirectoryPath = $profile | Split-Path
+$defaultProfileFilePath = "$defaultProfileDirectoryPath\$ProfileFileName"
+Copy-Item -Path $CustomProfilePath -Destination $defaultProfileFilePath
+Write-Output "Copied custom profile to '$defaultProfileFilePath'."
+
+if (-not ($profile -eq $defaultProfileFilePath))
 {
     Copy-Item -Path $CustomProfilePath -Destination $profile
     Write-Output "Additionaly copied to custom profile path: '$profile'."    
