4ec79997875c93885db34b48449ca3d30b250e20
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2022-07-05 08:36:53 +0200

    Change default subst drive letter.

--- Windows/Set-SubstDrive.ps1
+++ Windows/Set-SubstDrive.ps1
@@ -1,5 +1,5 @@
 # Sets for all users. This also ensures that it is available for remote sessions, e.g. Ansible.
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" -Name "R:" -Value "\??\D:\projects"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\DOS Devices" -Name "S:" -Value "\??\C:\Users\thoffmann\source"
 
 # Sets the local user:
 # Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" -Name "R Drive" -Value "subst R: D:\\projects"
