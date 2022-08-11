0438e37e8a831e3198d7701caa5e205481b4c436
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2022-08-11 12:23:53 +0200

    Improve powershell tab completion.

--- Windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,13 @@
 oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/pure.omp.json | Invoke-Expression
 Import-Module posh-git
+
+# Set tab completion with menu.
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+           [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+        }
+}
