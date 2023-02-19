1dd58c6c860ae16882e390c845a34e142a455403
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2023-02-19 17:40:43 +0100

    Add python install script.

---
+++ Windows/install_apps.py
@@ -0,0 +1,29 @@
+# Installs new applications.
+
+import subprocess
+
+apps = [
+    # Development.
+    "test.test",
+    "Microsoft.PowerShell",
+    # "Microsoft.VisualStudioCode"
+]
+
+for app in apps:
+    list_app_response = subprocess.run(f"winget list --exact --query {app}", \
+        stdout=subprocess.DEVNULL)
+
+    # Checks if the app is already installed.
+    if (list_app_response.returncode == 0):
+        print(f"Found '{app}'. Attempting to upgrade...")
+        output = subprocess.run(f"winget upgrade --exact --query {app}")
+
+        if output.returncode == 0:
+            print("Upgraded successfully.")
+        elif output.returncode == 2316632107:
+            print("No applicable update found.")
+        else:
+            print(f"Error: {output}.")
+
+    else:
+        print(f"Did not find installation of '{app}'. Attempting to install...")
