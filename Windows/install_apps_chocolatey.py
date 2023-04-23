cc7b4d0eb6b95c72522da5bda1106a67bcb9e3b5
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2023-04-23 12:50:15 +0200

    Refactor.

--- Windows/install_apps_chocolatey.py
+++ Windows/install_apps_chocolatey.py
@@ -6,20 +6,20 @@ if __name__ != "__main__":
     print("Running as module.")
     exit()
 
-apps_file_name = "chocolatey_apps.yml"
-
-print(":: Install Apps with Chocolatey ::")
-
 def main(args: argparse.Namespace) -> None:
     print(args)
 
     apps_to_install = get_apps_to_install(args)
     print(apps_to_install)
 
+    intall_apps(args, apps_to_install)
+
+def intall_apps(args: argparse.Namespace, apps: list[str]):
     if not args.y:
         print("To perform operations, provide the '-y' argument.")
         exit()
 
+    # TODO implement installation
 
 def get_apps_to_install(args: argparse.Namespace) -> list[str]:
     script_dir_path = os.getcwd()
@@ -45,10 +45,13 @@ def get_apps_to_install(args: argparse.Namespace) -> list[str]:
 
         return apps_to_install
 
+apps_file_name = "chocolatey_apps.yml"
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--private', action='store_true', help='install private apps.')
 parser.add_argument('--work', action='store_true', help='install work apps.')
 parser.add_argument('-y', action='store_true', help='confirm to perform destructive operations')
-
 args = parser.parse_args()
+
+print(":: Install Apps with Chocolatey ::")
 main(args)
