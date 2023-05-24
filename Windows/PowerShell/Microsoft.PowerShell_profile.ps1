657d01cdb4a93022ca7eb49b0d1939cc1b419e95
timdhoffmann <9947384+timdhoffmann@users.noreply.github.com>
2023-05-24 17:31:09 +0200

    Replace oh-my-posh with starship prompt.

--- Windows/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,20 @@
-oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/pure.omp.json | Invoke-Expression
+# Starship
+Invoke-Expression (&starship init powershell)
+
 Import-Module posh-git
 
 # Set tab completion with menu.
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
 
 # PowerShell parameter completion shim for the dotnet CLI
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
